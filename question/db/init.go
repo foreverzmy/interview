3c8fd3418e096def3e8e1c306f9ebd174771c65b
mervyn <mervyn@mervyndeMacBook-Pro.local>
2019-07-04 14:11:00 +0800

    fix: 数据库连接修改

--- question/db/init.go
+++ question/db/init.go
@@ -14,7 +14,6 @@ var DB *gorm.DB
 func init() {
 	var err error
 	// 初始化数据库连接
-	// DB, err = gorm.Open("mysql", "forever:zmyisno1@tcp(118.24.201.128:3306)/ereader?charset=utf8mb4&parseTime=true")
 	DB, err = gorm.Open("mysql", "root:zmyisno1@tcp(0.0.0.0:3306)/interview?charset=utf8mb4&parseTime=true")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
